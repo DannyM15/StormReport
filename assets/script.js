e5e67379cd5b1c69d8b95e295e16475861e3c877
DannyM15 <merinodanny15@gmail.com>
2024-02-03 15:42:32 -0800

    local storage working

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 var cityname = document.querySelector('#username')
 
 //var requestURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname}&appid=463d1cee038354c58ccc692c67d9b4d7`
+var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityname.value}&appid=463d1cee038354c58ccc692c67d9b4d7&units=imperial`
+
+
 
 function clickbtn(event) {
     event.preventDefault();
@@ -16,6 +19,7 @@ function clickbtn(event) {
                 dayIndex += 8
             }
         })
+
     event.preventDefault();
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname.value}&appid=463d1cee038354c58ccc692c67d9b4d7&units=imperial`)
         .then(res => res.json())
@@ -29,22 +33,41 @@ var searchButton = document.querySelector(".btn")
 
 searchButton.addEventListener("click", clickbtn)
 
+
+//LOCAL STORAGE 
+
 function saveSearch() {
-    var searchQuery = document.getElementById('#username');
+    var searchHistory;
+    var searchQuery = document.getElementById('username');
+
     if (typeof (Storage) !== "undefined") {
-        localStorage.setItem('lastSearch', searchQuery);
-        console.log("Search query saved: " + searchQuery);
+        if (localStorage.getItem('lastSearch')) {
+            searchHistory = JSON.parse(localStorage.getItem('lastSearch'));
+        } else {
+            searchHistory = [];
+        }
+
+        searchHistory.push(searchQuery.value)
+        localStorage.setItem('lastSearch', JSON.stringify(searchHistory));
+        console.log("Search query saved: " + searchQuery.value);
     } else {
         console.long("local storage is not supported by this browser.")
     }
 }
 
+// On page load, retrieve the last search query from local storage
 window.onload = function () {
-    var lastSearch = localStorage.getItem("lastSearch");
+    var lastSearch = JSON.parse(localStorage.getItem("lastSearch"));
     if (lastSearch) {
-        document.getElementById("#username") = lastSearch;
+        var searchHistory = document.querySelector('#repos-container');
+        for (var i = 0; i < lastSearch.length; i++) {
+            var button = document.createElement('button');
+            button.innerText = lastSearch[i];
+            searchHistory.appendChild(button)
+        }
         console.log("Last search query retrieved: " + lastSearch);
     }
 };
+
 
 

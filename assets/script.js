355f86f19c9d5ad166362c5e799f879b449a5bd7
DannyM15 <merinodanny15@gmail.com>
2024-02-04 22:10:35 -0800

    recent search buttons working

--- assets/script.js
+++ assets/script.js
@@ -62,6 +62,7 @@ window.onload = function () {
             var button = document.createElement('button');
             button.innerText = lastSearch[i];
             searchHistory.appendChild(button)
+            button.addEventListener("click", handleRecentSearch);
         }
         console.log("Last search query retrieved: " + lastSearch);
     }
@@ -75,21 +76,23 @@ function handleRecentSearch(event) {
     fetchWeatherData(cityName); // Call a function to fetch weather data using the city name
 }
 
-// Add event listeners to all recent search buttons
-var recentSearchButtons = document.querySelectorAll("#repos-container button");
-recentSearchButtons.forEach(function(button) {
-    button.addEventListener("click", handleRecentSearch);
-});
-
 // Function to fetch weather data using the provided city name
 function fetchWeatherData(cityName) {
-    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName.value}&appid=463d1cee038354c58ccc692c67d9b4d7&units=imperial`;
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=463d1cee038354c58ccc692c67d9b4d7&units=imperial`;
     fetch(apiURL)
         .then(res => res.json())
         .then(weatherData => {
             console.log(weatherData);
+            var dayIndex = 2
+            for (let i = 1; i < 6; i++) {
+                document.querySelector('#wind-' + i).textContent = "Wind Spped: " + weatherData.list[dayIndex].wind.speed
+                document.querySelector('#weather-' + i).textContent = "Temperature: " + weatherData.list[dayIndex].main.temp
+                document.querySelector('#humidity-' + i).textContent = "Humidity: " + weatherData.list[dayIndex].main.humidity
+                dayIndex += 8
+            }
+        })
             // Update the UI with the fetched weather data
             // ...
-        });
-}
+        };
+
 
